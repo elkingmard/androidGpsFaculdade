6d09814e52e5144246879525f899e11e5fe3ff4f
Ramon Moraes <ramon.moraes94@gmail.com>
2017-11-20 15:36:10 -0300

    initial configs on configAcitivity

--- app/src/main/java/com/example/ramonmoraes/gpsfaculdade/ConfigActivity.java
+++ app/src/main/java/com/example/ramonmoraes/gpsfaculdade/ConfigActivity.java
@@ -3,6 +3,9 @@ package com.example.ramonmoraes.gpsfaculdade;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class ConfigActivity extends AppCompatActivity {
 
@@ -12,10 +15,61 @@ public class ConfigActivity extends AppCompatActivity {
     private Integer tipoMapa; //1= vetorial , 2 = imagem satelite;
     private Boolean trafego;
 
+
+    private RadioButton btnPrecisao1;
+    private RadioButton btnPrecisao2;
+    private RadioButton btnPrecisao3;
+
+    private RadioButton btnVel1;
+    private RadioButton btnVel2;
+
+    private RadioButton btnMapType1;
+    private RadioButton btnMapType2;
+
+    private RadioButton btnOrientacao1;
+    private RadioButton btnOrientacao2;
+    private RadioButton btnOrientacao3;
+
+    private Button btnTrafego;
+
+    private Button btnSalvar;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        instanceButtons();
+
+        btnSalvar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                modifyAtributesValues();
+            }
+        });
+
+    }
+
+
+    public void instanceButtons(){
+
+        this.btnPrecisao1 = (RadioButton)findViewById(R.id.btnPrecisao1);
+        this.btnPrecisao2 = (RadioButton)findViewById(R.id.btnPrecisao2);
+        this.btnPrecisao3 = (RadioButton)findViewById(R.id.btnPrecisao3);
+
+        this.btnVel1 = (RadioButton)findViewById(R.id.btnVel1);
+        this.btnVel2 = (RadioButton)findViewById(R.id.btnVel2);
+
+        this.btnMapType1 = (RadioButton)findViewById(R.id.btnMapType1);
+        this.btnMapType2 = (RadioButton)findViewById(R.id.btnMapType2);
+
+        this.btnOrientacao1 = (RadioButton)findViewById(R.id.btnOrientacao1);
+        this.btnOrientacao2 = (RadioButton)findViewById(R.id.btnOrientacao2);
+        this.btnOrientacao3 = (RadioButton)findViewById(R.id.btnOrientacao3);
+
+        this.btnTrafego = (Button)findViewById(R.id.btnTrafego);
+        this.btnSalvar = (Button)findViewById(R.id.btnSalvar);
     }
 
     private void saveConfigPref(){
@@ -31,6 +85,26 @@ public class ConfigActivity extends AppCompatActivity {
     }
 
     private void modifyAtributesValues(){
+        if(btnPrecisao1.isChecked()) this.precisao=1;
+        if(btnPrecisao2.isChecked()) this.precisao=2;
+        if(btnPrecisao3.isChecked()) this.precisao=3;
+
+        if(btnVel1.isChecked()) this.velocidade=1;
+        if(btnVel2.isChecked()) this.velocidade=2;
+
+        if(btnOrientacao1.isChecked()) this.orientacao=1;
+        if(btnOrientacao2.isChecked()) this.orientacao=2;
+        if(btnOrientacao3.isChecked()) this.orientacao=3;
+
+        if(btnMapType1.isChecked()) this.tipoMapa=1;
+        if(btnMapType2.isChecked()) this.tipoMapa=2;
+
+        this.trafego = (btnTrafego.isActivated()) ? true : false;
+
+        saveConfigPref();
+    }
+
+    private void loadConfigPref(){
 
     }
 
